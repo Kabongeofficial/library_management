742499a0c9ceff42b59bb9891b4cf2890c943c9e
kabongeofficial <musakabonge01@gmail.com>
2023-10-10 12:55:49 +0300

    starting afresh

--- library_management/library_management/doctype/library_membership/library_membership.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, kabonge and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Library Membership", {
-// 	refresh(frm) {
-
-// 	},
-// });
