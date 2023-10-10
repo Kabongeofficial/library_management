32c60ef598f1774c345a405d78c8056d2c20f409
kabongeofficial <musakabonge01@gmail.com>
2023-10-10 15:05:19 +0300

    validating the full name field

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2023, kabonge and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LibraryMember(Document):
-	pass
+    # this method will run every time a document is saved
+    def before_save(self):
+        self.full_name = f'{self.first_name} {self.last_name or ""}'
